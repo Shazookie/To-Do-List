d2a439a574e260ce1ea61428aeeb6667806ac41b
Shazookie <seamusmsommers@gmail.com>
2026-02-14 15:43:31 -0500

    added text tracker from the field

--- script.js
+++ script.js
@@ -18,12 +18,27 @@ console.log(taskList);
 function addTask(event){
     // stops from reloading page 
     event.preventDefault();
-    // shows us the click worked
-    console.log("Button was clicked");
+
+        // shows us the click worked 
+        //console.log("Button was clicked");
+
+    //gets text 
+    const newText = taskText.value;
+
+    // doesnt allow empty strings
+    if (newText === "")return;
+
+
+        // shows us the text popping up in console
+        //console.log(newText);
         
 }
 
 // link variables to function events
 
 taskMake.addEventListener('click', addTask);
+
+
+//making the blue print for elements of the to do list 
+
 
